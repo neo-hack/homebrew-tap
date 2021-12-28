a1d823cbc55f676ed183d790aee17d70f9aff277
jiangWeixian <jiangweixian1994@gmail.com>
2021-12-28 23:27:36 +0800

    :sparkles:  pd.rb

---
+++ pd.rb
@@ -0,0 +1,13 @@
+# <projectname>.rb
+#
+class Pd < Formula
+  desc "A test formula"
+  homepage "http://www.example.com"
+  url "-----"
+  version "-----"
+  sha256 "-----"
+
+  def install
+    bin.install "<bin-name>"
+  end
+end
