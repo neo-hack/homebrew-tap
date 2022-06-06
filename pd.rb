5a8bfb4325d058ed607b4af3de7698ceb7c96e25
JW <jiangweixian1994@gmail.com>
2022-06-06 12:58:44 +0800

    updating formula for pd

--- pd.rb
+++ pd.rb
@@ -3,9 +3,9 @@
 class Pd < Formula
   desc "search local package.name directly"
   homepage "https://github.com/neo-hack/pd"
-  url "https://github.com/neo-hack/pd/releases/download/v0.2.0/pd-v0.2.0-x86_64-macos.tar.xz"
-  version "0.2.0"
-  sha256 "942b17bd4d575cdaf2f4a75018f2a08df0cbcee91a2ac8ad3914876d314d2523"
+  url "https://github.com/neo-hack/pd/releases/download/v0.3.0/pd-v0.3.0-x86_64-macos.tar.xz"
+  version "0.3.0"
+  sha256 "45eed81310309b4f14bd4208d95dc32915e9623216ddf08b5a9620463fbbfa3e"
 
   def install
     bin.install "pd"
